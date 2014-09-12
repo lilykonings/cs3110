ebc4fdf0750830e92eb35089987656475de6cb53
Lily <lilyc5459@gmail.com>
2014-09-11 20:15:45 -0400

    directory cleanup

--- ps1/ps1.ml
+++ ps1/ps1.ml
@@ -16,6 +16,7 @@ let rec is_mon_inc (lst:int list) =
 
 let is_unimodal lst =
 
+
 let rec powerset lst =
 	match lst with
 	| [] -> [[]]
@@ -34,6 +35,19 @@ let rev_int i =
 			if i<0 then -1*(reverse (-1*i) 0)
 			else reverse i 0
 
-let unflatten k l =
-	if k <= 0 then None
-	else let rec unflatten_helper i l =
+let unflatten k lst =
+	if k<=0 then None
+	else let rec unflatten_helper i a b = function
+  	| [] -> []
+  	| [x] -> (x::a)::b
+  	| h::t ->
+	  	if i<=1 then unflatten_helper k [] ((h::a)::b) t
+		  else unflatten_helper (i-1) (h::a) b t in
+		  	Some(List.rev(unflatten_helper k [] [] lst))
+
+
+
+
+
+
+
