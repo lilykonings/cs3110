ddb82de075bf01b2028d6c976c4f9ff7291c4814
Lily <lilyc5459@gmail.com>
2014-10-02 18:02:20 -0400

    insert is taking forever...

--- ps3/quadtree.ml
+++ ps3/quadtree.ml
@@ -17,17 +17,33 @@ exception OutOfBounds
 
 let new_tree (r:region) : 'a quadtree =
   match r with
-  | ((x0,y0),(x1,y1)) -> 
-    let a = Leaf((((x0+.x1)/.2.0,(y0+.y1)/.2.0),(x1,y1)),[]) in
-    let b = Leaf(((x0,(y0+.y1)/.2.0),((x0+.x1)/.2.0,y1)),[]) in
-    let c = Leaf(((x0,y0),((x0+.x1)/.2.0,(y0+.y1)/.2.0)),[]) in
-    let d = Leaf((((x0+.x1)/.2.0,y0),(x1,(y0+.y1)/.2.0)),[]) in
-      Node(r,a,b,c,d)
+    | ((x0,y0),(x1,y1)) -> 
+      let a = Leaf ((((x0+.x1)/.2.0,(y0+.y1)/.2.0),(x1,y1)),[]) in
+      let b = Leaf (((x0,(y0+.y1)/.2.0),((x0+.x1)/.2.0,y1)),[]) in
+      let c = Leaf (((x0,y0),((x0+.x1)/.2.0,(y0+.y1)/.2.0)),[]) in
+      let d = Leaf ((((x0+.x1)/.2.0,y0),(x1,(y0+.y1)/.2.0)),[]) in
+        Node (r,a,b,c,d)
         
 let rec insert (q: 'a quadtree) (c : coord) (s:'a) : 'a quadtree =
-  let r = match q with
-  | Node(((x0,y0),(x1,y1)),_,_,_,_) -> 
-  | Leaf(((x0,y0),(x1,y1)),_) ->
+  let find_quad x0 x1 y0 y1 c0 c1 =
+    if (c0<x0 || c0>x1) then 0
+    if (c1<y0 || c1>y1) then 0
+    else
+      if (c0>(x0+.x1)/.2.0) then
+        if (c1>(y0+.y1)/.2.0) then 1
+        else 4
+      else
+        if (c1>(y0+.y1)/.2.0) then 2
+        else 3
+  let r = match q,c with
+    | Node (((x0,y0),(x1,y1)),_,_,_,_), (c0,c1) -> find_quad x0 x1 y0 y1 c0 c1
+    | Leaf (((x0,y0),(x1,y1)),_), (c0,c1) -> find_quad x0 x1 y0 y1 c0 c1 in
+  if (r == 0) then
+    raise (OutOfBounds "Coordinates of object not within bounds of quadtree!")
+  else match q with
+    | Node () -> insert 
+    | Leaf (r,[]) -> Leaf (r, [(c,s)])
+    | Leaf (r,h::t) -> 
 							      
 let rec fold_quad (f: 'a -> (coord * 'b)  -> 'a) (a: 'a) (t: 'b quadtree): 'a =
   failwith "TODO"
