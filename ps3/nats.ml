7a41fc2155630a5740fa30b8bac64ea457a599af
Lily <lilyc5459@gmail.com>
2014-10-04 22:19:48 -0400

    problem 3, ex 1

--- ps3/nats.ml
+++ ps3/nats.ml
@@ -3,16 +3,55 @@
 module type NATN = sig
   type t
 
+  (* identity element for addition: a+0 === a *)
+  (* returns: same inputed natural number if added *)
   val zero : t
+
+  (* identity element for multiplication: a*1 === a *)
+  (* returns: same inputed natural number if multiplied *)
   val one : t
+
+  (* addition operator
+   * associative: (a+b)+c === a+(b+c)
+   * communitive: a+b === b+a *)
+  (* requires: two natural numbers to be added *)
+  (* returns: the sum also of type t *)
   val ( + ) : t -> t -> t
+
+  (* multiplication operator
+   * associative: (a*b)*c === a*(b*c)
+   * communitive: a*b === b*a
+   * distributive property over addition: a*(b+c) === (a*b)+(a*c) *)
+  (* requires: two natural numbers to be multiplied *)
+  (* returns: the product also of type t *)
   val ( * ) : t -> t -> t 
+
+  (* checks if a natural number is smaller than another
+   * transitive: a < b, b < c, then a < c *)
+  (* requires: two natural numbers to be compared *)
+  (* returns: true if the first is smaller than the second, false otherwise *)
   val ( < ) : t -> t -> bool
+
+  (* checks if two natural numbers are equal
+   * reflexive: a === a
+   * transitive: a === b, b === c, then a === c
+   * symmetric: a === b, b === a *)
+  (* requires: two natural numbers to be compared *)
+  (* returns: true if they are equal, false otherwise *)
   val ( === ) : t -> t -> bool
 			    
   exception Unrepresentable
 
+  (* converts a natural number to primitive int type *)
+  (* requires: the natural number to be converted *)
+  (* returns: the resulting int or an Unrepresentable error if
+   * the natural number cannot become an int *)
   val int_of_nat: t -> int
+
+  (* converts a primitive int type to a natural number *)
+  (* requires: an int to be converted *)
+  (* returns: the resulting natural number of an Unrepresentable
+   * error if the int cannot become a natural number *)
   val nat_of_int: int -> t
 end
 
