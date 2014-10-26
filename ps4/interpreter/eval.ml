11cb8d6528f580e68d2258f8e426cd34bc1c53d5
Lily <lilyc5459@gmail.com>
2014-10-25 21:04:39 -0400

    read_expression done

--- ps4/interpreter/eval.ml
+++ ps4/interpreter/eval.ml
@@ -13,43 +13,6 @@ and value =
 and binding = value ref Environment.binding
 and environment = value ref Environment.environment
 
-(* Parses a datum into an expression. *)
-let rec read_expression (input : datum) : expression =
-  match input with
-  | Atom (Identifier id) when Identifier.is_valid_variable id -> ExprVariable id
-  | Atom (Identifier id) -> failwith "Error: usage of keyword without arguments"
-  | Atom (Boolean b) -> ExprSelfEvaluating (SEBoolean b)
-  | Atom (Integer n) -> ExprSelfEvaluating (SEInteger n)
-  | Cons (datum, Nil) -> read_expression datum
-  | Cons (Atom (Identifier id), datum2) when Identifier.is_valid_variable id ->
-    ExprProcCall (ExprVariable id, List.map read_expression (list_of_cons d2))
-  | Cons (Atom (Identifier id), datum2) ->
-    match id, datum2 with
-    | "quote", d -> ExprQuote d
-    | "if", Cons (d1,Cons (d2,d3)) ->
-      ExprIf (read_expression d1, read_expression d2, read_expression d3)
-    | "if", _ -> failwith "Error: invalid arguments for if keyword"
-    | "lambda", Cons (d1,d2) ->
-      ExprLambda (list_of_cons d1, List.map read_expression (list_of_cons d2))
-    | "lambda", _ -> failwith "Error: invalid arguments for lambda keyword"
-    | "set!", Cons (d1,d2) -> ExprAssignment (d1, read_expression d2)
-    | "set!", _ -> failwith "Error: invalid arguments for set! keyword"
-    | "let", Cons (d1,d2) ->
-      ExprLet (List.map (fun (a,b) -> (a,read_expression b)) (list_of_cons d1),
-        List.map read_expression (list_of_cons d2))
-    | "let", _ -> failwith "Error: invalid arguments for let keyword"
-    | "let*", Cons (d1,d2) ->
-      ExprLet (List.map (fun (a,b) -> (a,read_expression b)) (list_of_cons d1),
-        List.map read_expression (list_of_cons d2))
-    | "let*", _ -> failwith "Error: invalid arguments for let* keyword"
-    | "letrec", Cons (d1,d2) ->
-      ExprLet (List.map (fun (a,b) -> (a,read_expression b)) (list_of_cons d1),
-        List.map read_expression (list_of_cons d2))
-    | "letrec", _ -> failwith "Error: invalid arguments for letrec keyword"
-    | "", _ -> failwith "Error: invalid keyword"
-  | Cons (_,_) -> failwith "Error: invalid cons arguments"
-  | _ -> failwith "Error: datum does not match expression grammar"
-
 (* Convert nested cons into a list *)
 (* Requires: datum should not be Atom and values stored in cons should all be same type *)
 (* Returns: a list of values in cons or an error if inputted an Atom *)
@@ -59,10 +22,70 @@ let rec list_of_cons (d : datum) : 'a list =
   | Cons (h,t) -> h::(list_of_cons t)
   | Nil -> []
 
+let variable_of_datum (d : datum) : variable =
+  match d with
+  | Atom (Identifier id) when Identifier.is_valid_variable id ->
+    Identifier.variable_of_identifier id
+  | _ -> failwith "Error: datum cannot be converted to variable"
+
+(* Parses a datum into an expression. *)
+let rec read_expression (input : datum) : expression =
+  match input with
+  | Atom (Identifier id) when Identifier.is_valid_variable id -> ExprVariable (Identifier.variable_of_identifier id)
+  | Atom (Identifier id) -> failwith "Error: usage of keyword without arguments"
+  | Atom (Boolean b) -> ExprSelfEvaluating (SEBoolean b)
+  | Atom (Integer n) -> ExprSelfEvaluating (SEInteger n)
+  | Cons (d, Nil) -> read_expression d
+  | Cons (Atom (Identifier id), d) when Identifier.is_valid_variable id ->
+    ExprProcCall (ExprVariable (Identifier.variable_of_identifier id), List.map read_expression (list_of_cons d))
+  | Cons (Atom (Identifier id), datum2) ->
+    begin
+      match Identifier.string_of_identifier id, datum2 with
+      | "quote", d -> ExprQuote d
+      | "if", Cons (d1,Cons (d2,d3)) ->
+        ExprIf (read_expression d1, read_expression d2, read_expression d3)
+      | "if", _ -> failwith "Error: invalid arguments for if keyword"
+      | "lambda", Cons (d1,d2) ->
+        ExprLambda (List.map variable_of_datum (list_of_cons d1), List.map read_expression (list_of_cons d2))
+      | "lambda", _ -> failwith "Error: invalid arguments for lambda keyword"
+      | "set!", Cons (d1,d2) -> ExprAssignment (variable_of_datum d1, read_expression d2)
+      | "set!", _ -> failwith "Error: invalid arguments for set! keyword"
+      | "let", Cons (d1,d2) ->
+        ExprLet (List.map (
+          fun x ->
+            match x with
+            | Cons (a,b) -> (variable_of_datum a,read_expression b)
+            | _ -> failwith "Error: invalid arguments for let keyword"
+        ) (list_of_cons d1), List.map read_expression (list_of_cons d2))
+      | "let", _ -> failwith "Error: invalid arguments for let keyword"
+      | "let*", Cons (d1,d2) ->
+        ExprLet (List.map (
+          fun x ->
+            match x with
+            | Cons (a,b) -> (variable_of_datum a,read_expression b)
+            | _ -> failwith "Error: invalid arguments for let* keyword"
+        ) (list_of_cons d1), List.map read_expression (list_of_cons d2))
+      | "let*", _ -> failwith "Error: invalid arguments for let* keyword"
+      | "letrec", Cons (d1,d2) ->
+        ExprLet (List.map (
+          fun x ->
+            match x with
+            | Cons (a,b) -> (variable_of_datum a,read_expression b)
+            | _ -> failwith "Error: invalid arguments for letrec keyword"
+        ) (list_of_cons d1), List.map read_expression (list_of_cons d2))
+      | "letrec", _ -> failwith "Error: invalid arguments for letrec keyword"
+      | _,_ -> failwith "Error: invalid keyword"
+    end
+  | Cons (_,_) -> failwith "Error: invalid cons arguments"
+  | _ -> failwith "Error: datum does not match expression grammar"
+
 (* Parses a datum into a toplevel input. *)
 let read_toplevel (input : datum) : toplevel =
   match input with
-  | 
+  | Atom (Boolean b) ->
+  | Atom (Integer n) ->
+  | Atom (Identifier id) when Identifier.is_valid_variable id -> 
+  | Atom (Identifier id) ->
 
 (* This function returns an initial environment with any built-in
    bound variables. *)
@@ -73,7 +96,7 @@ let initial_environment () : environment =
   ("cons",fun x y -> (x,y));
   ("+",);
   ("-",);
-  ("equal?",fun x y -> );
+  ("equal?",fun x y -> x = y);
   ("eval",fun x y -> eval x y)]
 
 (* Evaluates an expression down to a value in a given environment. *)
@@ -104,9 +127,9 @@ let eval (expression : expression) (env : environment) : value =
   | ExprLetRec (_, _) ->
 
 (* Finds value of variable in env if exists, otherwise return None *)
-let find (var : variable) (env : environment) : value option =
+(* let find (var : variable) (env : environment) : value option =
   try Some (List.assoc var env)
-  with Not_found -> None
+  with Not_found -> None *)
 
 (* Evaluates a toplevel input down to a value and an output environment in a
    given environment. *)
