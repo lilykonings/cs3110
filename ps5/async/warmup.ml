3a545367eb8c6fb30e19a090696e45ea9bcf2c9f
Lily <lilyc5459@gmail.com>
2014-11-05 22:50:49 -0500

    finished warmup

--- ps5/async/warmup.ml
+++ ps5/async/warmup.ml
@@ -1,7 +1,26 @@
 open Async.Std
 
+(* runs two concurrent functions on d when it becomes determined *)
+(* requires: a deferred computation d
+						 two functions that takes in the determined value of d *)
+(* returns: unit *)
 let fork d f1 f2 =
-  failwith "My mind is aglow with whirling, transient nodes of thought..."
+	ignore (d >>= f1);
+	ignore (d >>= f2);
+	()
 
+(* maps list through a given function,
+ * computing each element simultaneously *)
+(* requires: a list of type 'a elements
+						 a function that takes
+ * an 'a type argument and returns 'b Deferred.t *)
+(* returns: 'b list Deferred.t, each element is the output of
+ * f taking in the corresponding element in l *)
 let deferred_map l f =
-  failwith "Ditto."
+	let helper d acc =
+		d >>= (fun x ->
+			acc >>= (fun a ->
+				return (x::a)
+			)
+		) in
+	List.fold_right helper (List.map f l) (return [])
