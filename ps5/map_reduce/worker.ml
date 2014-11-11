430232c5beed210166d33b0032278d0ebb877b79
Ed Fang <eyf6@cornell.edu>
2014-11-10 21:51:29 -0500

    Pulled the modules outside of the run function

--- ps5/map_reduce/worker.ml
+++ ps5/map_reduce/worker.ml
@@ -1,24 +1,22 @@
 open Async.Std
 
 module Make (Job : MapReduce.Job) = struct
+    module Request = Protocol.WorkerRequest (Job)
+    module Response = Protocol.WorkerResponse (Job)
 
   (* see .mli *)
   let run (r: Reader.t) (w: Writer.t) : unit Deferred.t =
     (*get request from controller*)
-    let module Request = Protocol.WorkerRequest (Job) in 
-    let module Response = Protocol.WorkerResponse (Job) in 
 
   let handle (job_result: 'a Deferred.t) =
-    let open Response in  
+
     try_with (fun () -> job_result) 
       >>| function
         | Core.Std.Result.Ok (a) -> (match a with 
-          | [] as lst -> send w (MapResult lst)
-          | (key,inter) :: t as lst -> send w (MapResult lst)) 
-        | Core.Std.Result.Error _ -> send w (JobFailed "Job failed") in 
+          | [] as lst -> Response.send w (Response.MapResult lst)
+          | (key,inter) :: t as lst -> Response.send w (Response.MapResult lst)) 
+        | Core.Std.Result.Error _ -> Response.send w (Response.JobFailed "Job failed") in 
 
-      let open Request in 
-      let open Response in 
       Request.receive r 
         >>= function 
           | `Ok job_request -> (match job_request with  
@@ -27,7 +25,7 @@ module Make (Job : MapReduce.Job) = struct
                 try_with (fun () -> Job.reduce (key,inters))
                 >>| function
                   | Core.Std.Result.Ok result -> Response.send w (Response.ReduceResult result)
-                  | Core.Std.Result.Error _ -> Response.send w (JobFailed "Job failed")) 
+                  | Core.Std.Result.Error _ -> Response.send w (Response.JobFailed "Job failed")) 
           | `Eof -> return () 
   
 end
